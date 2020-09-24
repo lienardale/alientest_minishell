8c425e4c9eba4b1eca1b6d4421502bbb9fd27365
alienard <alienard@student.42.fr>
2020-09-24 10:34:12 +0200

    more tests

--- exit.sh
+++ exit.sh
@@ -9,6 +9,12 @@ exit 165411651651615132132 | echo test
 exit asdas | echo test
 exit 1 2 3 | echo test
 
+echo test| exit
+echo test| exit 165411651651615132132
+echo test | exit asdas
+echo test | exit 1 2 3
+echo test | exit 1 a 2 3
+
 cd pouet ; exit ;
 
 # echo "ls hahsda
